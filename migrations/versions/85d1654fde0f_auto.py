0b7bdd46e366ef8257ccece2de25a4624fa933f4
dCoder-coder <avianthon7@gmail.com>
2026-03-02 22:04:20 +0530

    fix(lint): sort imports in new alembic migration

--- migrations/versions/85d1654fde0f_auto.py
+++ migrations/versions/85d1654fde0f_auto.py
@@ -7,8 +7,8 @@ Create Date: 2026-03-02 16:21:54.825770
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 
 # revision identifiers, used by Alembic.
